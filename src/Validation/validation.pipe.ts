1ee4f2f45efe1307e795790637c1969a1d63cfac
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-05-08 20:40:19 +0630

    chore: update validation pipe

--- src/Validation/validation.pipe.ts
+++ src/Validation/validation.pipe.ts
@@ -1,19 +1,16 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
-   async transform(value: any, {metatype}: ArgumentMetadata) {
-        if (!metatype || !this.toValidate(metatype)) {
-            return value
-        }
-
-        const object= new metatype(value)
-        const errors = await validate(object)
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
     }
+  }
 
-    private toValidate (metatype: Function): boolean {
-        const types: Function[] = [String, Boolean, Number, Array, Object]
+  private toValidate(metatype: Function): boolean {
+    const types: Function[] = [String, Boolean, Number, Array, Object];
 
-        return !types.includes(metatype)
-    }
+    return !types.includes(metatype);
+  }
 }
